3ec9c1eacf0795f512aac02212ffafbcdae8cbc3
Aric S. Jones <aric.jones@student.msdmartinsville.org>
2025-01-29 11:47:43 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/nextjs-github-pages",
+  basePath: "/Sunori_NextJS",
   images: {
     unoptimized: true,
   },
